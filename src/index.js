66de2063d82cd44e0e0558c13547a83438f3275c
Spandana Pokla <spandana.pokla@hcl.com>
2021-04-12 19:54:22 +0530

    Modified index.js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Header from './common/Header.js';
+import Header from './common/header/Header.js';
 import 'typeface-roboto';
 
 // ReactDOM.render(
